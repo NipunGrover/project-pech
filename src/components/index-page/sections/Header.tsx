380c85173a7d30ad0d808ae6bbf12d9843aad2cc
j2fyi <207806977+j2fyi@users.noreply.github.com>
2025-07-30 19:55:13 -0400

    fix: lint issues for build

--- src/components/index-page/sections/Header.tsx
+++ src/components/index-page/sections/Header.tsx
@@ -7,9 +7,7 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
 
 export function Header() {
   return (
