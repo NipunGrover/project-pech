7393b08673df4d35535a1a259143252f3a67c02a
j2fyi <207806977+j2fyi@users.noreply.github.com>
2025-08-27 19:07:57 -0400

    chore: create typography based on figma

---
+++ src/components/ui/heading.tsx
@@ -0,0 +1,98 @@
+import type { VariantProps } from "class-variance-authority";
+import type { PolymorphicComponentPropWithRef } from "@/components/polymorphic";
+
+import { Slot } from "@radix-ui/react-slot";
+import { cva } from "class-variance-authority";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export const HeadingVariants = cva("m-0 font-semibold", {
+	variants: {
+		size: {
+			"xs": "text-2xl leading-8 tracking-normal", // 24px / 32px
+			"sm": "text-[1.875rem] leading-[2.375rem] tracking-normal", // 30px / 38px
+			"md": "text-4xl leading-11 tracking-[-0.02em]", // 36px / 44px
+			"lg": "text-5xl leading-[3.75rem] tracking-[-0.02em]", // 48px / 60px
+			"xl": "text-[3.75rem] leading-[4.5rem] tracking-[-0.02em]", // 60px / 72px
+			"2xl": "text-[4.5rem] leading-[5.625rem] tracking-[-0.02em]", // 72px / 90px
+		},
+		color: {
+			accent: "text-accent-11",
+			warning: "text-warning-11",
+			success: "text-success-11",
+			error: "text-error-11",
+			gray: "text-gray-11",
+		},
+		highContrast: {
+			true: "",
+			false: "",
+		},
+	},
+	compoundVariants: [
+		{
+			color: "gray",
+			highContrast: true,
+			className: "text-gray-12",
+		},
+		{
+			color: "accent",
+			highContrast: true,
+			className: "text-accent-12",
+		},
+		{
+			color: "warning",
+			highContrast: true,
+			className: "text-warning-12",
+		},
+		{
+			color: "success",
+			highContrast: true,
+			className: "text-success-12",
+		},
+		{
+			color: "error",
+			highContrast: true,
+			className: "text-error-12",
+		},
+	],
+	defaultVariants: {
+		size: "md",
+		color: "gray",
+		highContrast: false,
+	},
+});
+
+type BaseHeadingProps = VariantProps<typeof HeadingVariants>;
+
+type HeadingProps<C extends React.ElementType = "h2">
+	= | ({ asChild?: false } & PolymorphicComponentPropWithRef<C, BaseHeadingProps>)
+		| ({ asChild: true; children: React.ReactNode } & BaseHeadingProps);
+
+type HeadingComponent = (<C extends React.ElementType = "h2">(
+	props: HeadingProps<C>
+) => React.ReactNode) & { displayName?: string };
+
+const Heading: HeadingComponent = <C extends React.ElementType = "h2">(
+	{ ...props },
+) => {
+	const { asChild, size, color, highContrast, ...others } = props;
+
+	const className = cn(
+		HeadingVariants({ size, color, highContrast }),
+		(others as unknown as { className: string }).className,
+	);
+
+	if (asChild) {
+		return <Slot {...others} className={className} />;
+	}
+
+	const { as = "h2", ...rest } = others as { as: C } & Record<string, unknown>;
+
+	const Component = as;
+
+	return <Component {...rest} className={className} />;
+};
+
+Heading.displayName = "Heading";
+
+export { Heading };
